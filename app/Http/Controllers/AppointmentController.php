d0d657d1e21f148a8fa124e8e2dab4afb09f1d77
Lucca da Silva <lcsilva@nelogica.com.br>
2021-02-19 22:47:02 -0300

    Adds validation rules for the appointment route

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Appointment;
+use Illuminate\Support\Facades\Validator as IlluminateValidator;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
     public function store(Request $request)
     {
+        $validation = IlluminateValidator::make($request->all(), [
+            'date' => 'required|date_format:Y-m-d',
+            'description' => 'string',
+        ]);
+
+        if ($validation->fails()) {
+            return $this->badRequest($validation->messages());
+        }
+
         return Appointment::create($request->all());
     }
 }
