81743a4de752164397331fa0299be5aa3379386b
Lucca da Silva <lcsilva@nelogica.com.br>
2021-02-19 22:18:28 -0300

    Fix routes syntax

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'pet',
 ], function () {
-    Route::get('/', 'PetController@index');
-    Route::get('/{pet}', 'PetController@show');
-    Route::post('/', 'PetController@store');
-    Route::put('/{pet}', 'PetController@update');
-    Route::delete('/{pet}', 'PetController@delete');
+    Route::get('/', 'App\Http\Controllers\PetController@index');
+    Route::get('/{pet}', 'App\Http\Controllers\PetController@show');
+    Route::post('/', 'App\Http\Controllers\PetController@store');
+    Route::put('/{pet}', 'App\Http\Controllers\PetController@update');
+    Route::delete('/{pet}', 'App\Http\Controllers\PetController@delete');
 });
 
 Route::group([
     'prefix' => 'appointment',
 ], function() {
-    Route::post('/{pet}', 'AppointmentController@store');
+    Route::post('/{pet}', 'App\Http\Controllers\AppointmentController@store');
 });
